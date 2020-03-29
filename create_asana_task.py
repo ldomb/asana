fb40bc609e6c5462046e77860eb74efb2aac1e3f
Laurent Domb <ldo@amazon.com>
2020-03-29 14:38:26 -0400

    asana create task

---
+++ create_asana_task.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+import requests
+import pyfiglet
+from dotenv import load_dotenv
+load_dotenv()
+
+
+### Load token and workspace 
+auth_token = os.getenv('AUTH_TOKEN')
+workspace = os.getenv('WORKSPACE')
+path = os.getenv('PATHTOFILE')
+
+ascii_banner = pyfiglet.figlet_format("Asana Task")
+print(ascii_banner)
+
+### input needed 
+taskname = raw_input("Enter task name : ")
+due_on = raw_input("Enter due on date 2020-04-20: ")
+assignee = raw_input("Enter the assignee's email: ")
+notes = raw_input("Enter your description: ")
+
+#filename = raw_input("Enter the filename from your message : ")
+#pathtofile = path+filename
+
+## open meeting file with task info
+#with open(pathtofile, 'r') as file:
+#    notes = file.read().replace('\n', '')
+
+hed = {'Authorization': 'Bearer ' + auth_token}
+data = {"data": {"name": taskname, 
+        "completed": "false",
+        "due_on": due_on,
+        "notes": notes,
+        "assignee": assignee,
+        "workspace": workspace}}
+
+url = 'https://app.asana.com/api/1.0/tasks'
+response = requests.post(url, json=data, headers=hed)
+
+#print(response)
+#print(response.json())
