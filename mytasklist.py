1ed5e504dfeb5ad5a8d0b43ac57b3a2df4dacca2
Laurent Domb <ldo@amazon.com>
2020-03-30 08:26:18 -0400

    working on tables

--- mytasklist.py
+++ mytasklist.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from json2html import *
 import os
 import json
 import requests
@@ -13,8 +14,10 @@ mytasklist = os.getenv('MYTASKLIST')
 project = os.getenv('PROJECTID')
 hed = {'Authorization': 'Bearer ' + auth_token}
 
-url = 'https://app.asana.com/api/1.0/user_task_lists/'+mytasklist+'/tasks?opt_fields=name,notes,due_on'
+url = 'https://app.asana.com/api/1.0/user_task_lists/'+mytasklist+'/tasks?opt_fields=due_on,notes,name'
 response = requests.get(url,headers=hed)
 dict = json.loads(response.text)
 dict_nice = json.dumps(dict, indent=2)
 print(dict_nice)
+#tasklist=json2html.convert(json = dict_nice)
+#print(tasklist)
