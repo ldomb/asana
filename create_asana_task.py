04d05331e1b84eab828fb4489de683da71319210
Laurent Domb <ldo@amazon.com>
2020-04-07 08:41:27 -0400

    added disclaimer

--- create_asana_task.py
+++ create_asana_task.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+# Copyright 2020 Laurent Domb
+#
+# File: create_asana_task.py 
+#
+# Asana scripts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Asana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Asana scripts.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os
 import json
